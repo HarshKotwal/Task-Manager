f8a11c1fece782323efd50c22bab7b78042ae07d
HarshKotwal <harshkotwal1608@gmail.com>
2025-10-16 14:47:36 +0530

    Normalize line endings to show changes properly

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const { createTask } = require("./types");
 const app = express();
 app.use(express.json());
 
-app.post("/todo", function (req, res) {});
+app.post("/todo", function (req, res) {
+  const createPayload = req.body;
+  const parsePayload = createTask.safeParse(createPayload);
+  if (!parsePayload.success) {
+    res.status(411).json({
+      msg: "You sent the wrong inputs",
+    });
+    return;
+  }
+});
 
 app.get("/", function (req, res) {});
 
