a53d1c09856a9a9f1a2087c38c3f281d43cf1beb
HarshKotwal <harshkotwal1608@gmail.com>
2025-10-16 15:33:05 +0530

    update BE

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { createTask } = require("./types");
+const { createTask, updateTask } = require("./types");
 const app = express();
 app.use(express.json());
 
 app.post("/todo", function (req, res) {
   const createPayload = req.body;
-  const parsePayload = createTask.safeParse(createPayload);
+  const parseCreate = createTask.safeParse(createPayload);
   if (!parsePayload.success) {
     res.status(411).json({
       msg: "You sent the wrong inputs",
@@ -16,4 +16,13 @@ app.post("/todo", function (req, res) {
 
 app.get("/", function (req, res) {});
 
-app.put("/update", function (req, res) {});
+app.put("/update", function (req, res) {
+  const updatePayload = req.body;
+  const parseUpdate = updateTask.safeParse(updatePayload);
+  if (!parseUpdate.success) {
+    res.status(411).json({
+      msg: "You sent wrong inputs",
+    });
+    return;
+  }
+});
